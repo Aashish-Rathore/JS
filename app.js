b4a3cf7e6cb81e744f9ee8f9d7aaa3557bd0b66c
Aashish-Rathore <aashishrathore1309@gmail.com>
2024-07-28 21:49:10 +0530

    add todo

--- app.js
+++ app.js
@@ -597,9 +597,71 @@
 // ..........................................................................
 
 
+// this keyword:-
+// let btn = document.querySelector("button");
+// btn.addEventListener("click",function(){
+// this.style.backgroundColor="yellow";
+// });
+
+
+
+
+// .........................................................................
+
+
+
+// let form = document.querySelector("form");
+// form.addEventListener("submit",function(event){
+//     event.preventDefault();
+
+//     let user = document.querySelector("#user");
+//     let pass = document.querySelector("#pass");
+
+//     console.log(user.value);
+//     console.log(pass.value);
+
+//     alert(`Hi ${user.value}, your password is set to ${pass.value}`)
+// });
+
+
+
+
+
+// ........................................................................
+
+
 
 let btn = document.querySelector("button");
-btn.addEventListener("click",function(){
-this.style.backgroundColor="yellow";
-});
+let ul = document.querySelector("ul");
+let inp =document.querySelector("input");
 
+btn.addEventListener("click",function(){
+    let item = document.createElement("li");
+    item.innerText = inp.value;
+
+    let delbtn=document.createElement("button");
+    delbtn.innerText="delete";
+    delbtn.classList.add("delete");
+
+    item.appendChild(delbtn);
+    ul.appendChild(item);
+    console.log(inp.value);
+    inp.value="";
+})
+
+ul.addEventListener("click",function(event){
+    if(event.target.nodeName == "BUTTON"){
+        let listItem = event.target.parentElement;
+        listItem.remove();
+        console.log("deleted");
+    }
+})
+
+// let delbtns = document.querySelectorAll(".delete");
+// for(delbtn of delbtns){
+//     delbtn.addEventListener("click", function(){
+//         let par = this.parentElement;
+//         console.log(par);
+//         par.remove();
+//     })
+// }
