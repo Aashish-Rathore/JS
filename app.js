f40236e2dd85094d077001921f574a96982c504a
Aashish-Rathore <aashishrathore1309@gmail.com>
2024-08-01 21:45:50 +0530

    add code

--- app.js
+++ app.js
@@ -677,16 +677,42 @@
 
 // Promises-->
 
-function savetoDb(data){
-    return new Promise((resolve,reject)=>{
-        let internetSpeed=Math.floor(Math.random()*10);
-        if(internetSpeed > 4){
-            resolve("success : data was saved");
-        }
-        else{
-            reject("failure : weak connection");
-        }
-    })
-}
+// function savetoDb(data){
+//     return new Promise((resolve,reject)=>{
+//         let internetSpeed=Math.floor(Math.random()*10);
+//         if(internetSpeed > 4){
+//             resolve("success : data was saved");
+//         }
+//         else{
+//             reject("failure : weak connection");
+//         }
+//     })
+// }
+
+// ..................................................................................
+
+
+
+
+// Async and await-->
+
+// h1=document.querySelector("h1");
+
+// function changeColor(color,delay){
+//     return new Promise((resolve,reject) => {
+//         setTimeout(()=>{
+//             h1.style.color = color;
+//             console.log(`color changed to ${color}!`);
+//             resolve("color changed!")
+//         },delay);
+//     });
+// }
+
+// async function demo(){
+//     await changeColor("red",1000);
+//     await changeColor("orange",1000);
+//     await changeColor("green", 1000)
+//     changeColor("blue" , 1000);
+// }
 
 // ..................................................................................
