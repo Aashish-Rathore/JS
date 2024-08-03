adc1b3b7ae44d5c7432a2f2aafe81408bcd91292
Aashish-Rathore <aashishrathore1309@gmail.com>
2024-08-03 23:13:26 +0530

    add code

--- app.js
+++ app.js
@@ -747,3 +747,32 @@
 
 
 // ......................................................................
+
+
+
+
+
+ 
+// using Fetch with async / await--->
+
+
+let url="https://catfact.ninja/fact";
+
+async function getFacts(){
+    try{
+        let res=await fetch(url);
+        let data = await res.json();
+        console.log(data.fact);
+
+        let res2=await fetch(url);
+        let data2 = await res2.json();
+        console.log(data2.fact);
+    } catch(e){
+        console.log("error -", e);
+    }
+
+    console.log("bye");
+}
+   
+
+// ......................................................................
