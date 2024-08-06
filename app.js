15f9fd2a5506b67ac75a40201a8a6175c4dc57ad
Aashish-Rathore <aashishrathore1309@gmail.com>
2024-08-06 22:51:28 +0530

    add code

--- app.js
+++ app.js
@@ -841,3 +841,27 @@
 
 
 // ....................................................................
+
+
+
+
+// AXIOS sending Headers--->
+const url = "https://icanhazdadjoke.com/";
+
+async function getJokes() {
+    try{
+        const config = { headers : { Accept : "application/json"} };
+        let res = await axios.get(url , config);
+        console.log(res.data);
+    }catch(err){
+        console.log(err);
+    }
+}
+
+
+// ......................................................................
+
+
+
+
+// Activity using Query Strings
